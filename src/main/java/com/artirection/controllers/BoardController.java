25bf9dd34c93993271ca2706e0319488f0ebc613
KDH <kth5541915@gmail.com>
2023-11-29 00:20:15 +0900

    test

--- src/main/java/com/artirection/controllers/BoardController.java
+++ src/main/java/com/artirection/controllers/BoardController.java
@@ -1,17 +1,12 @@
 package com.artirection.controllers;
 
-import java.sql.Timestamp;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.artirection.dto.BoardDTO;
 import com.artirection.services.BoardService;
 
-import commons.EncryptionUtils;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
